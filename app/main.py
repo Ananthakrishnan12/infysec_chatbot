9830882f0e2fea307066213899a97b26a7c29abf
Ananthakrishnan <ananthmle@gmail.com>
2026-03-12 17:43:36 +0530

    Fix CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from app.routers.chatbot import router as chatbot_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="AI Course Chatbot")
 
 app.include_router(chatbot_router, prefix="/api")
-
+# ✅ CORS CONFIGURATION
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def health():
     return {"status": "Chatbot Running"}
